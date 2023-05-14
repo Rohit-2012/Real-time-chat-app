2d78fc79b0d3bc1aef1045cd5d3e0081853e2328
Rohit Kirti <126654177+Rohit-2012@users.noreply.github.com>
2023-05-14 11:11:16 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 //instance of Server class
 const io = new Server (server, {
     cors:{
-        origin: "https://naklitwitter1.onrender.com/signIn:3000",
+        origin: "http://localhost:3000",
         methods: ["GET", "POST"]
     }
 })
