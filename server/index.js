655122fe0b5ca3ee2afced9461651ae2d7ea520b
Rohit Kirti <126654177+Rohit-2012@users.noreply.github.com>
2023-05-15 12:08:40 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app)
 //instance of Server class
 const io = new Server (server, {
     cors:{
-        origin: "http://localhost:3000",
+        origin: "*",
         methods: ["GET", "POST"]
     }
 })
